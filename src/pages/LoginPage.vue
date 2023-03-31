c93e39ceeeed49b6c7d6baf2c098917fca96aef5
IrynaShvets <irinashwets@ukr.net>
2023-03-31 17:22:47 +0300

    fix login validate

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -4,15 +4,15 @@
       <form @submit.prevent="handleSubmit" novalidate>
         <div class="flex flex-col mx-auto md:w-96 w-full">
           <h1 class="text-2xl font-bold mb-4 text-center">Login</h1>
-          <span class="text-[#ff0012]" v-if="errorsStatus">
-            {{ (errorsStatus).toString() }}
-          </span>
           <div class="flex flex-col gap-2 mb-4">
             <label for="email" class="required">Email</label>
             <input id="email" name="email" type="email" class="form-input" required v-model="email" />
             <span class="text-[#ff0012]" v-if="errors.email">
               {{ (errors.email).toString() }}
             </span>
+            <span class="text-[#ff0012]" v-if="errorsStatus">
+            {{ (errorsStatus).toString() }}
+          </span>
           </div>
 
           <div class="flex flex-col gap-2 mb-4">
@@ -21,6 +21,9 @@
             <span class="text-[#ff0012]" v-if="errors.password">
               {{ (errors.password).toString() }}
             </span>
+            <span class="text-[#ff0012]" v-if="errorsStatus">
+            {{ (errorsStatus).toString() }}
+          </span>
           </div>
 
           <div class="border-t h-[1px] my-6"></div>
@@ -72,25 +75,13 @@ export default {
       this.login(userData).then(() => { this.$router.push({ name: "posts" }); }).catch((errors) => {
 
         switch (errors.status) {
-          case 400:
-            this.errorsStatus = 'API | Nothing to display, data Not Found';
-            break
 
           case 401:
-            localStorage.removeItem('token')
-            this.errorsStatus = 'API | Please login again or Session Expired'
-            break
-
-          case 403:
-            this.errorsStatus = 'API | Access denied or data Not Found'
-            break
-
-          case 404:
-            this.errorsStatus = 'API | Dataset not found or data Not Found'
+            this.errors.email = errors.data.email;
+            this.errors.password = errors.data.password;
             break
 
           case 422:
-            this.errorsStatus = 'API | Validation error. Unprocessable Content'
             this.errors.email = errors.data.errors.email;
             this.errors.password = errors.data.errors.password;
             break
